49f67cce66663e4f926dc68bbd7aa67455a551b9
Tawhid-Komol <tawhid15-2861@diu.edu.bd>
2022-03-11 23:59:34 +0600

    update

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,12 +12,63 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0A0E21),
-        title: const Center(child: Text('BMI CALCULATOR')),
-      ),
-      body: const Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0A0E21),
+          title: const Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    colour: const Color(0xFF1A1B2C),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    colour: const Color(0xFF1A1B2C),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+              child: Card(
+                colour: const Color(0xFF1A1B2C),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    colour: const Color(0xFF1A1B2C),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    colour: const Color(0xFF1A1B2C),
+                  ),
+                ),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class Card extends StatelessWidget {
+  Card({required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
